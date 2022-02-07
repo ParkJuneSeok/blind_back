47abe43c0aeaa05ac26b553dee17e6da5fc8554a
Raccoon <vawoojs@naver.com>
2022-02-08 00:11:51 +0900

    First Commit

--- src/main/java/com/blind/back/blind_back/member/vo/MemberVO.java
+++ src/main/java/com/blind/back/blind_back/member/vo/MemberVO.java
@@ -4,8 +4,14 @@ import lombok.Data;
 
 @Data
 public class MemberVO {
-    private String memId;
-    private String memPw;
-    private String memNick;
-    private String insDate;
+    private int     memNo;          // 회원 고유 코드
+    private String  memId;          // 회원 아이디
+    private String  memPw;          // 회원 비번
+    private String  memNick;        // 회원 닉네임
+    private String  insDate;        // 가입일
+    private String  updDate;        // 정보 수정일
+
+    // 시크릿 정보 //
+    private String grade;       // 어드민에서 가져오는 직책 정보
+    private String department;  // 어드민에서 가져오는 부서 정보
 }
