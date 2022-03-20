1786bd12d3521e79219bb37c5e46c57156d409d5
raccoon <vawoojs@naver.com>
2022-03-21 08:01:00 +0900

    Security 권한

--- src/main/java/com/blind/back/blind_back/config/WebSecurityConfig.java
+++ src/main/java/com/blind/back/blind_back/config/WebSecurityConfig.java
@@ -32,6 +32,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .permitAll()
                 .and()
             .logout()
+                .logoutUrl("/member/logout")
                 .permitAll();
     }
 
